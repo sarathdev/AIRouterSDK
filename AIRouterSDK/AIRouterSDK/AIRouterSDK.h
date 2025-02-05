d157dd66430b978edb60b9f7906a08cd71e9aba7
Sarath Sasi <sarath.sasi@thoughtfocus.com>
2025-02-05 12:59:50 +0530

    Initial Commit AI ROuterSDK

---
+++ AIRouterSDK/AIRouterSDK/AIRouterSDK.h
@@ -0,0 +1,18 @@
+//
+//  AIRouterSDK.h
+//  AIRouterSDK
+//
+//  Created by Sarath Sasi on 05/02/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AIRouterSDK.
+FOUNDATION_EXPORT double AIRouterSDKVersionNumber;
+
+//! Project version string for AIRouterSDK.
+FOUNDATION_EXPORT const unsigned char AIRouterSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AIRouterSDK/PublicHeader.h>
+
+
---
+++ AIRouterSDK/AIRouterSDKTests/AIRouterSDKTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIRouterSDKTests.swift
+//  AIRouterSDKTests
+//
+//  Created by Sarath Sasi on 05/02/25.
+//
+
+import Testing
+@testable import AIRouterSDK
+
+struct AIRouterSDKTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
